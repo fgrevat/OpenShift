95c8acd6eeb04b440143cf7c8fd93991a54f5428
fgrevat <84853014+fgrevat@users.noreply.github.com>
2021-12-11 11:48:52 +0100

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Bonjour Monde ! php version is " . PHP_VERSION . "\n";
 print "Learning OpenShift is too much fun!!\n";
 ?>
